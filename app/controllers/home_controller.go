e6caa86d5dc696865b4b32bb8c49eae43e34a874
ProjectAkhirBelajarMembuatAplikasiAndroidPemulaDicoding <khairililmi2468@gmail.com>
2024-08-29 07:10:11 +0700

    push 11

--- app/controllers/home_controller.go
+++ app/controllers/home_controller.go
@@ -1,10 +1,18 @@
 package controllers
 
 import (
-    "net/http"
-    "fmt"
+	"net/http"
+
+	"github.com/unrolled/render"
 )
 
-func Home(w http.ResponseWriter, r *http.Request){
-    fmt.Fprint(w,"Selamat datang di page toko")
+func Home(w http.ResponseWriter, r *http.Request) {
+	render := render.New(render.Options{
+		Layout: "layout",
+	})
+
+	_ = render.HTML(w, http.StatusOK, "home", map[string]interface{}{
+		"title": "Home Title",
+		"body":  "Home Description",
+	})
 }
--- app/routes.go
+++ app/routes.go
@@ -1,10 +1,16 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/khairililmi2468gmailcom/toko_online_golang/app/controllers"
 )
 func (server *Server) initializeRoutes(){
-    server.Router = mux.NewRouter()
-    server.Router.HandleFunc("/", controllers.Home).Methods("GET")
+   server.Router = mux.NewRouter()
+	server.Router.HandleFunc("/", controllers.Home).Methods("GET")
+
+	staticFileDirectory := http.Dir("./assets/")
+	staticFileHandler := http.StripPrefix("/public/", http.FileServer(staticFileDirectory))
+	server.Router.PathPrefix("/public/").Handler(staticFileHandler).Methods("GET")
 }
